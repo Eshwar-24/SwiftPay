c44a68f811cc6a8d1232ddc2e6cd475ed3827bc5
Eshwar-24 <saieshwarmanchala42@gmail.com>
2026-05-27 13:34:27 +0530

    new project

---
+++ src/main/java/com/hackathon/SwiftPay/SwiftPayApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.SwiftPay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwiftPayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwiftPayApplication.class, args);
+	}
+
+}
